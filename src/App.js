4c04f7bfd0e68be2bc500c8cd7ae2be3329e3784
Andrew Eames <ajerxs@gmail.com>
2022-07-05 18:54:36 -0400

    Added Tailwind and React Unicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello world!</h1>
     </div>
   );
 }
