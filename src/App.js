28649e3b090b7baa9240f545433132bd03e97bb6
Andrew Eames <ajerxs@gmail.com>
2022-07-17 21:23:10 -0400

    Added state to App.js and useEffect hook for fetch

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons'
 import getFormattedWeatherData from './services/weatherService';
-// import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data);
-  };
+  const [query, setQuery] = useState({q: 'berlin'})
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
 
-  fetchWeather();
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const data = await getFormattedWeatherData({...query, units}).then(data => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units])
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
